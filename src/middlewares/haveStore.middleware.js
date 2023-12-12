a07e67552ecde6def3ec294cc2316f7fc3550ea4
Ramiro Gumma <ramirogumma@hotmail.com>
2023-12-12 12:18:36 -0300

    Modificacion de haveStore.middleware -> 'OK'

--- src/middlewares/haveStore.middleware.js
+++ src/middlewares/haveStore.middleware.js
@@ -1,17 +1,18 @@
-import UserManager from "../persistencia/DAOs/user.posgresql.js";
-const userManager = new UserManager();
-const haveStore = (req, res, next) => {
+import StoreManager from "../persistencia/DAOs/stores.postresql.js";
+const storeManager = new StoreManager();
+const haveStore = async (req, res, next) => {
   const user = req.user;
   if (!user) {
     return res.redirect(307, "/login");
   }
   //Comprobamos si el usuario tiene una tienda asociada a su cuenta de usuario
-  const store = userManager.getUserById(user.id);
-  if (!store) {
+  const store = await storeManager.getStoreOfUser(user.id);
+
+  if (store.error) {
     return res.redirect(300, "/create-store");
   }
-  //   Si posee store pasa
-  store && next();
+  // Si posee store pasa
+  store && store.id && next();
 };
 
 export default haveStore;
