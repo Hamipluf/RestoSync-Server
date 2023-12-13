2bf72c4702af9ebd349fd09c2c606ad9a12f7859
Ramiro Gumma <ramirogumma@hotmail.com>
2023-12-13 11:33:31 -0300

    Retirado de informacion sensible del user hacia la respuesta user.controllers

--- src/contollers/users.controllers.js
+++ src/contollers/users.controllers.js
@@ -1,6 +1,7 @@
 import customResponses from "../utils/customResponses.js";
 import UserManager from "../persistencia/DAOs/user.posgresql.js";
 import authManager from "../utils/authManager.js";
+import usersServices from "../services/users.services.js";
 const user = new UserManager();
 const url_production = process.env.URL_PRODUCCION;
 // Todos los users
@@ -57,30 +58,21 @@ export const getOneById = async (req, res) => {
       .json(customResponses.badResponse(405, "Metodo no permitido"));
   }
   try {
-    const userDB = await user.getUserById(id);
-    if (userDB.error) {
+    const user = await usersServices.getUserById(id);
+    if (user.error) {
       return res
         .status(400)
-        .json(customResponses.badResponse(400, userDB.message));
+        .json(customResponses.badResponse(400, user.message));
     }
-    for (const key in userDB) {
-      if (typeof userDB[key] === "string") {
-        userDB[key] = userDB[key].trim();
+    for (const key in user) {
+      if (typeof user[key] === "string") {
+        user[key] = user[key].trim();
       }
     }
-    console.log(userDB);
-    const userResponse = {
-      id: userDB.id,
-      name: userDB.name,
-      lastname: userDB.last_name,
-      username: userDB.username,
-      email: userDB.email,
-      role: userDB.role,
-      photos: userDB.photos,
-    };
+
     res
       .status(200)
-      .json(customResponses.responseOk(200, "User encontrado", userResponse));
+      .json(customResponses.responseOk(200, "User encontrado", user));
   } catch (error) {
     console.error("Error al obtener los registros:", error);
     return res
@@ -96,7 +88,7 @@ export const register = (req, res) => {
       .json(customResponses.badResponse(405, "Método no permitido"));
   }
   const user = req.user;
-  if ("error" in user) {
+  if (user.error) {
     return res
       .status(400)
       .send(
@@ -104,19 +96,10 @@ export const register = (req, res) => {
       );
   }
   const token = authManager.generateToken(user);
-  const userResponse = {
-    id: user.id,
-    name: user.name,
-    last_name: user.last_name,
-    email: user.email,
-    username: user.username,
-    role: user.role,
-    photos: user.photos,
-    tasks: user.tasks,
-  };
+
   res.json(
     customResponses.responseOk(200, "User registrado con exito", {
-      userResponse,
+      user,
       token,
     })
   );
@@ -138,9 +121,7 @@ export const login = (req, res) => {
   }
   const token = authManager.generateToken(user);
 
-  res.json(
-    customResponses.responseOk(200, "Bienvenido", { user, token })
-  );
+  res.json(customResponses.responseOk(200, "Bienvenido", { user, token }));
 };
 // Autentica y recupera el user loggeado
 export const authUser = (req, res) => {
